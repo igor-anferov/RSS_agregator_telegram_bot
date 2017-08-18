ae3875ee78317f000acf101e385d93cbdba378a6
igor-anferov <igor-anferov@mail.ru>
2017-08-18 22:19:23 +0300

    RSS updating added

---
+++ bot.go
@@ -0,0 +1,2 @@
+bot_token := "441870254:AAHHaQbPt7abuqN97pD5nxGbtKhRIUUZGCI"
+api_url := "https://api.telegram.org/bot" + bot_token + "/"
--- rss.go
+++ rss.go
@@ -3,15 +3,47 @@ package main
 import (
 	"fmt"
 	"github.com/SlyMarbo/rss"
-        "github.com/go-sql-driver/mysql"
+    //"github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func main() {
 
-	result, err := rss.Fetch("https://meduza.io/rss/all")
-	if err != nil {
-		fmt.Println(err)
+	rssFeedUrls := []string {
+		"http://gazeta.ru/export/rss/lenta.xml",
+		"https://tvrain.ru/export/rss/all.xml",
+		"http://interfax.ru/rss.asp",
+		"https://buzzfeed.com/index.xml",
+		"http://feeds.bbci.co.uk/news/world/rss.xml",
+		"http://news.rambler.ru/rss/world/",
+		"https://meduza.io/rss/all",
+		"http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
 	}
 
-	fmt.Println(result)
+	rssFeeds := make([]*rss.Feed, 0, len(rssFeedUrls))
+	for e := range rssFeedUrls {
+		result, err := rss.Fetch(rssFeedUrls[e])
+		if err != nil {
+			panic(err)
+		}
+		result.Refresh = time.Now()
+		result.Items = nil
+		result.Unread = 0
+		rssFeeds = append(rssFeeds, result)
+	}
+
+	for true {
+		for e := range rssFeeds {
+			err := rssFeeds[e].Update()
+			if err != nil {
+				panic(err)
+			}
+			for i := range rssFeeds[e].Items {
+				fmt.Println(rssFeeds[e].Items[i])
+			}
+			rssFeeds[e].Refresh = time.Now()
+			rssFeeds[e].Items = nil
+			rssFeeds[e].Unread = 0
+		}
+	}
 }
